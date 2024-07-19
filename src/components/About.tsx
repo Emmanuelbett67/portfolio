71bed1465fc79ec43da5595c8aad40755758693c
Joseph-Gicuguma <josephgicuguma@gmail.com>
2024-07-19 21:25:54 +0300

    made the chhanges to the about me page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@
 import { Paragraph } from "@/components/Paragraph";
 import Image from "next/image";
 
+
 import { motion } from "framer-motion";
 
 export default function About() {
@@ -71,47 +72,18 @@ export default function About() {
 
       <div className="max-w-4xl">
         <Paragraph className=" mt-4">
-          Hey there, I&apos;m John Doe - a passionate developer, avid writer,
-          and a connoisseur of awesome design. Welcome to my corner of the
-          digital world!
+          I&apos;m Emmanuel, a Junior Data Analyst who loves turning raw data into useful insights.
+          Welcome to my corner of the digital world!
         </Paragraph>
         <Paragraph className=" mt-4">
-          Since the early days of my journey, I&apos;ve been captivated by the
-          art of crafting exceptional digital experiences. As a developer, I
-          thrive on turning lines of code into functional and elegant solutions.
-          My goal is to not just create software, but to build digital marvels
-          that seamlessly merge form and function.
+          I&apos;ve always been fascinated by how data can tell stories and help make better decisions. I enjoy exploring datasets, finding trends, and uncovering patterns. I&apos;m good with Python, SQL, and data visualization tools like Tableau, which help me make sense of complex data.
         </Paragraph>
 
         <Paragraph className=" mt-4">
-          But my journey doesn&apos;t stop at coding. With a heart full of words
-          and a mind brimming with ideas, I&apos;ve ventured into the realm of
-          writing. From tech articles that unravel complex concepts to creative
-          tales that ignite the imagination, I weave words to inform, entertain,
-          and inspire.
+          When I&apos;m not working with data, I&apos;m out exploring the world. I love playing football, hiking, and biking. I&apos;m also quite a car enthusiast. These adventures keep me energized and curious, helping me come back with fresh ideas and a clear mind. I also keep up with the latest tech trends and work on personal data projects for fun.
         </Paragraph>
         <Paragraph className=" mt-4">
-          What sets me apart is my unwavering appreciation for design. I believe
-          that aesthetics and usability go hand in hand. My eye for awesome
-          design ensures that every project I undertake not only works
-          flawlessly under the hood but also looks stunning on the surface.
-        </Paragraph>
-        <Paragraph className=" mt-4">
-          Through this website, I aim to share my insights, experiences, and
-          creations with you. Whether you&apos;re a fellow developer seeking
-          solutions, a fellow writer in search of inspiration, or simply someone
-          who appreciates the finer aspects of design, there&apos;s something
-          here for you.
-        </Paragraph>
-        <Paragraph className=" mt-4">
-          Join me on this journey of bytes and narratives, logic and creativity,
-          code and prose. Together, we can explore the boundless possibilities
-          of technology and storytelling, all while reveling in the sheer beauty
-          of thoughtful design.
-        </Paragraph>
-        <Paragraph className=" mt-4">
-          Thank you for being here, and I can&apos;t wait to embark on this
-          adventure with you.
+          On this website, I&apos;ll share my insights, experiences, and the stories I find in data. Whether you&apos;re into data, adventure, or just curious about what I do, there&apos;s something here for you. Join me on this journey of discovery, both in data and the great outdoors. Thanks for stopping by, and I can&apos;t wait to share this adventure with you.
         </Paragraph>
       </div>
     </div>
